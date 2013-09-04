6933bc98160aec7cf0f55a881b6c16f5fbd5573c
Julian Bohorquez <julianarmando@gmail.com>
2013-09-04 02:03:17 -0500

    Ahora envia html pero aun no estoy usando plantillas

--- app/views.py
+++ app/views.py
@@ -1,11 +1,18 @@
 # Create your views here.
+
+
+#Agregado adicional al curso
+from django.shortcuts import render_to_response
+
+
 from django.http import HttpResponse
 
 def home(request):
-	return HttpResponse("Hola mundo")
+	return HttpResponse("Hola mundo en holaaa")
 
 def hola(request):
-	return HttpResponse("Hola mundo en holaaa")
+	prueba = "<html><body style='color:red;'>hola</body></html>"
+	return HttpResponse(prueba)
 
 def numero(request, numerito):
 	return HttpResponse("Hola el numero es %s" % numerito)
